cc07065df393bd1f9e44b94a4bf1778685c9f15d
saumyasaxenaa <saumya.mins@gmail.com>
2020-06-28 10:38:26 -0700

    Arrays

--- Arrays/Dynamic Array.py
+++ Arrays/Dynamic Array.py
@@ -1,33 +1,27 @@
 class Array:
     def __init__(self):
         self.length = 0
-        self.data = {}
+        self.data = []
 
-    def __str__(self):
-        return str(self.__dict__)
+    # def __str__(self):
+    #     return str(self.__dict__)
 
     def get(self, index):
         return self.data[index]
 
     def push(self, item):
-        self.data[self.length] = item
+        self.data.append(item)
         self.length += 1
-        return self.length
 
     def pop(self):
-        lastitem = self.data[self.length-1]
-        del self.data[self.length-1]
-        self.length -= 1
-        return lastitem
+        self.data.pop()
 
     def delete(self, index):
-        deleteditem = self.data[index]
-        for i in range(index, self.length-1):
-            self.data[i] = self.data[i+1] # shifting the items to the left by one
-        del self.data[self.length-1]
-        self.length -= 1
-        return deleteditem
+        self.data.remove(self.get(index))
+        return self.data
 
+    def printArr(self):
+        return self.data
 
 
 arr = Array()
@@ -36,8 +30,7 @@ arr.push('hello')
 arr.push('you')
 arr.push('are')
 arr.push('nice')
-print(arr)
+print(arr.printArr())
 arr.delete(1)
-print(arr)
 arr.delete(2)
-print(arr)
+print(arr.printArr())
